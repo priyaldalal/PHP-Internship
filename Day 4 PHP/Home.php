3499d44ae179b4171afab79e8d4f6895f63e46f4
Priyal Dalal <84765716+priyaldalal@users.noreply.github.com>
2021-05-29 12:48:55 +0530

    Day 4 Theame

---
+++ Day 4 PHP/Home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Priyal Tech</title>
+<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+<link href="http://fonts.googleapis.com/css?family=Coda:400,800" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<?php
+include './Themepart/Menue.php';
+include './Themepart/logo1.php';
+?>
+
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+            <div><img src="pic2.jpg" width="920" height="450" alt="" /></div>
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">Welcome to Priyal Tech </a></h2>
+				<p class="meta"><span class="date">June 01, 2021</span><span class="posted">Posted by <a href="#">Someone</a></span></p>
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+					<p>Hello Every one My NAME IS PRIYAL</p>
+				</div>
+			</div>
+			
+			<div style="clear: both;">&nbsp;</div>
+		</div>
+		<!-- end #content -->
+                <?php
+                        include './Themepart/sidebar.php';
+                ?>
+		
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+</div>
+<?php
+                include './Themepart/footer.php';
+?>
+<!-- end #footer -->
+</body>
+</html>
---
+++ Day 4 PHP/Themepart/footer.php
@@ -0,0 +1,3 @@
+<div id="footer">
+	<p>Copy Rigths 2021. Website Manage BY Priyal Dalal</p>
+</div>
